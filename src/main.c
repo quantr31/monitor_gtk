9dd22105082c92b05e41cf71dd34fe3fc4ed52cd
QuanTrieu <20092151@student.hut.edu.vn>
2020-07-01 18:03:59 +0700

    add GPIO level change detection and GPIO output

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+/**************************************************
+ * Monitoring screen application with initial function: 
+ * - Setup the timer, then count down from the timer 
+ * - Creat another thread to receive input from dry contact
+ * using external circuit, change the display when the contact closes
+ * - Using button on GTK/glade design to control relay module 
+ * Author: Quan T.V.V 
+ * Company: LFS 
+ * Date: July 1st 2020
+ * ************************************************/
+
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,13 +16,20 @@
 #include <bcm2835.h>
 #include <glib.h>
 
-#define PIN RPI_GPIO_P1_11
-GMutex mutex_lock;
+//LED output pin 
+#define PIN_OUT RPI_GPIO_P1_11
+#define PIN_IN RPI_GPIO_P1_15
+//mutex lock to protect access to memory when threading
+GMutex mutex_lock_1;
+GMutex mutex_lock_2;
+uint8_t is_contact;
 
+//widgets struct
 typedef struct {
     GtkWidget *timer_lbl;
     GtkWidget *clock_lbl;
     GtkWidget *light_lbl;
+    GtkWidget *contact_lbl;
     GtkWidget *input_hrs;
     GtkWidget *input_mnt;
     GtkWidget *input_sec;
@@ -87,27 +105,90 @@ void on_btn_count_clicked(GtkButton *button, app_widgets *widgets)
     g_timeout_add_seconds(1, (GSourceFunc)countdown, widgets);
 }
 
+void relay1_control(app_widgets *widgets)
+{
+      bcm2835_gpio_fsel(PIN_OUT, BCM2835_GPIO_FSEL_OUTP);
+      while(1)
+      {
+            bcm2835_gpio_write(PIN_OUT, HIGH);
+            delay(500);
+            bcm2835_gpio_write(PIN_OUT, LOW);
+            delay(500);
+            while(gtk_events_pending()){gtk_main_iteration();}
+      } 
+      bcm2835_close(); 
+}
+
+void on_btn_light_clicked(GtkButton *button, app_widgets *widgets)
+{
+        g_thread_new(NULL, (GThreadFunc)relay1_control, (app_widgets*) widgets);
+}
+/*********
 gboolean update_func(app_widgets *widgets)
 {
-    g_mutex_lock(&mutex_lock);
+    g_mutex_lock(&mutex_lock_1);
     widgets->data = 1;
     gchar *text1 = g_strdup_printf("%d", widgets->data);
     gtk_label_set_text(GTK_LABEL(widgets->light_lbl), text1);
     g_free(text1);
-    g_mutex_unlock(&mutex_lock);
+    g_mutex_unlock(&mutex_lock_1);
     return TRUE;
 }
-    
-int LED_control(app_widgets *widgets)
+********/
+gboolean display_dry_contact(app_widgets *widgets)
 {
-    if(!bcm2835_init())
-    return 0;
-    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+    g_mutex_lock(&mutex_lock_2);
+    widgets->data = 2;
+    gchar *text2 = g_strdup_printf("%d", widgets->data);
+    gtk_label_set_text(GTK_LABEL(widgets->contact_lbl),text2);
+    g_free(text2);
+    g_mutex_unlock(&mutex_lock_2);
+    return TRUE;
+}
+
+gboolean display_dry_contact_1(app_widgets *widgets)
+{
+    g_mutex_lock(&mutex_lock_2);
+    widgets->data = 3;
+    gchar *text2 = g_strdup_printf("%d", widgets->data);
+    gtk_label_set_text(GTK_LABEL(widgets->contact_lbl),text2);
+    g_free(text2);
+    g_mutex_unlock(&mutex_lock_2);
+    return TRUE;
+}
+
+void check_dry_contact(app_widgets *widgets)
+{
+    // Set RPI pin P1-15 to be an input
+    bcm2835_gpio_fsel(PIN_IN, BCM2835_GPIO_FSEL_INPT);
+    //  with a pullup
+    bcm2835_gpio_set_pud(PIN_IN, BCM2835_GPIO_PUD_UP);
     while(1)
     {
-    bcm2835_gpio_write(PIN, HIGH);
+            is_contact = bcm2835_gpio_lev(PIN_IN);
+            if(is_contact == 1)
+            {
+            gdk_threads_add_idle((GSourceFunc)display_dry_contact, widgets);
+            }
+            else if (is_contact == 0)
+            {
+            gdk_threads_add_idle((GSourceFunc)display_dry_contact_1, widgets);
+            }
+            delay(500);
+            while(gtk_events_pending()){gtk_main_iteration();}
+    }
+    bcm2835_close();
+}
+
+/*********************
+int LED_control(app_widgets *widgets)
+{
+    bcm2835_gpio_fsel(PIN_OUT, BCM2835_GPIO_FSEL_OUTP);
+    while(1)
+    {
+    bcm2835_gpio_write(PIN_OUT, HIGH);
     delay(500);
-    bcm2835_gpio_write(PIN, LOW);
+    bcm2835_gpio_write(PIN_OUT, LOW);
     delay(500);
     
     gdk_threads_add_idle((GSourceFunc)update_func, widgets);
@@ -116,6 +197,7 @@ int LED_control(app_widgets *widgets)
     bcm2835_close();
     return 1;
     }
+********************/
     
 int main(int argc, char *argv[])
 {    
@@ -124,8 +206,15 @@ int main(int argc, char *argv[])
     GtkWidget       *window;
     app_widgets *widgets = g_slice_new(app_widgets);
     
+    //init bcm2835 lib
+    if(!bcm2835_init())
+    return 1
+    ;
     //init GPIO thread
-    g_thread_new(NULL, (GThreadFunc)LED_control, (app_widgets*) widgets);
+    
+    //test thread
+    //g_thread_new(NULL, (GThreadFunc)LED_control, (app_widgets*) widgets);
+    g_thread_new(NULL, (GThreadFunc)check_dry_contact, (app_widgets*) widgets);
     
     gtk_init(&argc, &argv);
     builder = gtk_builder_new_from_file("glade/window_main.glade");
@@ -139,6 +228,7 @@ int main(int argc, char *argv[])
     widgets->timer_lbl = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_text"));
     widgets->clock_lbl = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_clock"));
     widgets->light_lbl = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_light"));
+    widgets->contact_lbl = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_contact"));
     //widgets->data = 60;
 
     gtk_builder_connect_signals(builder, widgets);
